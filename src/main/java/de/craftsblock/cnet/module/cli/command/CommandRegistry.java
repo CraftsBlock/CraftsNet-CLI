bc6ecf452a847a1d93fe7dddc38f090f4adf3838
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2025-12-08 22:31:20 +0100

    feat: Add new ways to find cmds

--- src/main/java/de/craftsblock/cnet/module/cli/command/CommandRegistry.java
+++ src/main/java/de/craftsblock/cnet/module/cli/command/CommandRegistry.java
@@ -3,16 +3,17 @@ package de.craftsblock.cnet.module.cli.command;
 import de.craftsblock.cnet.module.cli.CraftsNetCLI;
 import de.craftsblock.craftsnet.logging.Logger;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * The {@link CommandRegistry} class manages and provides access to registered commands.
  * It allows commands to be retrieved, checked for existence, and executed.
-
+ *
  * @author Philipp Maywald
  * @author CraftsBlock
- * @version 1.0.0
+ * @version 1.1.0
  * @see Command
  * @since 1.0.0
  */
@@ -27,7 +28,7 @@ public class CommandRegistry {
      * @param addon The CraftsNet instance which instantiates this command registry.
      */
     public CommandRegistry(CraftsNetCLI addon) {
-        this.logger = addon.getLogger();
+        this.logger = addon.getCraftsNet().getLogger();
     }
 
     /**
@@ -36,9 +37,36 @@ public class CommandRegistry {
      * @param name The name of the command to retrieve.
      * @return The command with the specified name or a new command if it doesn't exist.
      */
-    @NotNull
-    public Command getCommand(String name) {
+    public @NotNull Command getCommand(String name) {
         return commands.computeIfAbsent(name, Command::new);
+    }
+
+    /**
+     * Finds a specific command by the name or the alias.
+     *
+     * @param command The name or alias of the command to find.
+     * @return The matching command or {@code null} if not found.
+     * @since 1.0.1
+     */
+    public @Nullable Command findCommand(String command) {
+        return findCommand(command, null);
+    }
+
+    /**
+     * Finds a specific command by the name or the alias.
+     *
+     * @param command The name or alias of the command to find.
+     * @param orElse  The alternativ value if no matching command could be found.
+     * @return The matching command or the {@code orElse} value.
+     * @since 1.0.1
+     */
+    public @Nullable Command findCommand(String command, Command orElse) {
+        return commands.getOrDefault(
+                command,
+                commands.values().stream()
+                        .filter(cmd -> cmd.isAlias(command))
+                        .findFirst().orElse(orElse)
+        );
     }
 
     /**
@@ -58,20 +86,24 @@ public class CommandRegistry {
      * @param args The arguments to pass to the command executor.
      */
     public void perform(String name, String[] args) {
-        Command command;
-
-        if (commands.containsKey(name)) command = commands.get(name);
-        else command = commands.values().stream()
-                .filter(cmd -> cmd.isAlias(name))
-                .findFirst()
-                .orElse(null);
+        Command command = findCommand(name);
 
         if (command == null) {
-            logger.warning("This command was not found!");
+            logger.warning("No command or alias named %s found!", name);
             return;
         }
 
         command.getExecutor().onCommand(command, name, args, logger);
+    }
+
+    /**
+     * Get a map of all currently registered commands mapped to thier name.
+     *
+     * @return The command map.
+     * @since 1.0.1
+     */
+    public @NotNull ConcurrentHashMap<String, Command> getCommands() {
+        return commands;
     }
 
 }
